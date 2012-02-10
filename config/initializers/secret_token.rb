cc9808b61535856cc87bf0765169d0fef421aa68
Casey Watts <casey.s.watts@gmail.com>
2012-02-10 02:03:31 -0500

    rails begins.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yalefacebookplus::Application.config.secret_token = 'c132a2d0613b3dcd14680c8cb9a254d97d2feaf1be9b8b8f36e2eb7c8681dd362ee9b7f3ea2d04683516ebaafe6eab8c803c55614f23d639edb189d352ed622e'
